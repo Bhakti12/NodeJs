5389b9656719253f50d0000d8454d97cd5b30cb2
Bhakti12 <sanghanibhakti922@gmail.com>
2023-06-06 17:06:44 +0530

    using mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,53 @@
-const Sequelize = require('sequelize');
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('nodejs_demo', 'root', '', {
-  dialect:'mysql',
-  host: 'localhost'
-});
+// let _db;
 
-module.exports = sequelize;
+// const MongoConnect = callback => {
+//   MongoClient.connect('mongodb+srv://sanghanibhakti:O2ISBXIiFmlJA0yl@cluster0.jhthccl.mongodb.net/Demo?retryWrites=true&w=majority')
+// .then(client=>{
+//   console.log('connected!');
+//   _db = client.db();
+//   callback();
+// })
+// .catch(err=>console.log(err));
+// };
+
+// const getDb = () => {
+//   if(_db){
+//     return _db;
+//   }
+//   throw 'No database found!';
+// }
+
+// exports.MongoConnect = MongoConnect;
+// exports.getDb = getDb;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://sanghanibhakti:O2ISBXIiFmlJA0yl@cluster0.jhthccl.mongodb.net/Demo?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
